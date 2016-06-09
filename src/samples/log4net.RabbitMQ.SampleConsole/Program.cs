579d9234e2006c88b1ed47122789032ad105fe47
Michael Denny <micdenny@gmail.com>
2016-06-09 12:47:44 +0200

    add sample console

---
+++ src/samples/log4net.RabbitMQ.SampleConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using log4net.Config;
+
+[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+
+namespace log4net.RabbitMQ.SampleConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var log = LogManager.GetLogger(typeof(Program));
+
+            log.Debug("Application start");
+
+            for (int i = 0; i < 10; i++)
+            {
+                log.Debug($"Debug {i}");
+
+                if (i % 2 == 0)
+                {
+                    try
+                    {
+                        throw new Exception("dummy exception!!");
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error($"Error {i}: {ex.Message}", ex);
+                    }
+                }
+            }
+
+            log.Debug("Application end");
+
+            LogManager.Shutdown();
+
+            Console.WriteLine("Press enter to exit the application...");
+            Console.ReadLine();
+        }
+    }
+}
